a540f5367f77a91a1b94809f5f661d0ec08bd598
Master3307 <hirmerkorbinian122@gmail.com>
2026-07-02 20:31:41 +0200

    avatar decoration adjusts

--- src/lib/avatar-decoration.js
+++ src/lib/avatar-decoration.js
@@ -1,11 +1,11 @@
 const AVATAR_DECORATIONS = [
   {
     src: "https://cdn.discordapp.com/avatar-decoration-presets/a_8c18ac604dd50ec43d571f18af63c79f.png",
-    weight: 70,
+    weight: 40,
   },
   {
     src: "https://cdn.discordapp.com/avatar-decoration-presets/a_cc83efd93ecd6e41857449c3c0ef9b22.png",
-    weight: 20,
+    weight: 14,
   },
   {
     src: "https://cdn.discordapp.com/avatar-decoration-presets/a_780cd1b7e878dce85d20c7ee495a86fe.png",
@@ -13,8 +13,16 @@ const AVATAR_DECORATIONS = [
   },
   {
     src: "https://cdn.discordapp.com/avatar-decoration-presets/a_c3cffc19e9784f7d0b005eecdf1b566e.png",
-    weight: 15,
+    weight: 55,
   },
+  {
+    src: "https://cdn.discordapp.com/avatar-decoration-presets/a_298fa5dff77b0ff58a8930a51c1a44d4.png",
+    weight: 10,
+  },
+  {
+    src: "https://cdn.discordapp.com/avatar-decoration-presets/a_401041801efe2618180692bfdd7a66c3.png",
+    weight: 10,
+  }
 ];
 
 export function getRandomAvatarDecoration() {
