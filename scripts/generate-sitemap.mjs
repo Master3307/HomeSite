9befeff68e5cdfc90c3230ac2fb51984d0be69ed
Master3307 <hirmerkorbinian122@gmail.com>
2026-07-07 19:54:51 +0200

    chore(sitemap): refactor sitemap addition for improved readability

--- scripts/generate-sitemap.mjs
+++ scripts/generate-sitemap.mjs
@@ -2,9 +2,8 @@ import { Sitemap } from "@mongez/sitemap";
 
 const sitemap = new Sitemap("https://home.master3307.org");
 
-sitemap
-  .add("/")
-  .add("/card")
-  .add("/about");
+sitemap.add("/");
+sitemap.add("/card");
+sitemap.add("/about");
 
 await sitemap.saveTo("./public/sitemap.xml");
