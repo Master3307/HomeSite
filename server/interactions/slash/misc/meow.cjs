e9c66c851d30844d833282c2e450b40d9d82c316
Master3307 <hirmerkorbinian122@gmail.com>
2026-08-03 01:06:09 +0200

    fix(meow): update command execution to use message.reply instead of interaction.reply

--- server/interactions/slash/misc/meow.cjs
+++ server/interactions/slash/misc/meow.cjs
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder().setName("meow").setDescription("Meow?"),
 
-  async execute(interaction) {
-    await interaction.reply("Meow Meow! <a:CATO_Danse:1533609328433561760>");
+  execute(message, args) {
+    message.reply({ content: "Meow Meow! <a:CATO_Danse:1533609328433561760>" });
   },
 };
