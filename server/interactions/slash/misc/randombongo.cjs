c0fbbd195b792ab0199fd86c4be3aeff261b68a0
Master3307 <hirmerkorbinian122@gmail.com>
2026-08-05 00:48:23 +0200

    feat(randombongo): add random cat GIF to the collection with description

--- server/interactions/slash/misc/randombongo.cjs
+++ server/interactions/slash/misc/randombongo.cjs
@@ -10,6 +10,10 @@ const catImages = [
     description: 'A random Cat from "Cataas"!',
   },
   {
+    url: "https://cataas.com/cat/gif/says/Meow%20Meow!",
+    description: "Random Cat GIF! :3",
+  },
+  {
     url: "https://user.uploads.dev/file/4f588aa32b1fcbaae594574504f917f0.png",
     description: "The Crimson Crew!",
   },
