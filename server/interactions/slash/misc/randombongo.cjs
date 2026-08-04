feaf81f273b9720e4a6bb0f9ea2c2753e8e9da8e
Master3307 <hirmerkorbinian122@gmail.com>
2026-08-04 14:24:19 +0200

    feat(randombongo): enhance random cat photo command with detailed descriptions and improved embed response

--- server/interactions/slash/misc/randombongo.cjs
+++ server/interactions/slash/misc/randombongo.cjs
@@ -1,8 +1,34 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 const catImages = [
-  "https://cdn.discordapp.com/avatars/815532619831574538/a52c158fe145686419c5aa347930b920.webp?size=1024",
-  "https://cataas.com/cat",
+  {
+    url: "https://cdn.discordapp.com/avatars/815532619831574538/a52c158fe145686419c5aa347930b920.webp?size=1024",
+    description: "Nigel Secret",
+  },
+  {
+    url: "https://cataas.com/cat/cute/says/Meow%20Meow!",
+    description: 'A random Cat from "Cataas"!',
+  },
+  {
+    url: "https://user.uploads.dev/file/4f588aa32b1fcbaae594574504f917f0.png",
+    description: "The Crimson Crew!",
+  },
+  {
+    url: "https://user.uploads.dev/file/f5b189f29cc78e699040fe8fa4ea2abc.gif",
+    description: "<:surprised:1534166841151197338>",
+  },
+  {
+    url: "https://user.uploads.dev/file/74ceb65265ec16df78a2b048df3c1856.png",
+    description: "Bongo Cat Jumpscare!",
+  },
+  {
+    url: "https://user.uploads.dev/file/e9055ed59969899ca050073e4f925abe.jpg",
+    description: "this is beetle :3",
+  },
+  {
+    url: "https://user.uploads.dev/file/7cf4129ce93b02040d47167c6e7dea99.jpg",
+    description: "<@1322220385411928136> sent this one :D",
+  },
 ];
 
 module.exports = {
@@ -11,7 +37,8 @@ module.exports = {
     .setDescription("Random Cat Photo."),
 
   async execute(interaction) {
-    let randomImage = catImages[Math.floor(Math.random() * catImages.length)];
+    const randomItem = catImages[Math.floor(Math.random() * catImages.length)];
+    let randomImage = randomItem.url;
 
     if (randomImage.includes("cataas.com/cat")) {
       const url = new URL(randomImage);
@@ -20,7 +47,8 @@ module.exports = {
     }
 
     const embed = new EmbedBuilder()
-      .setTitle("Here is your random image!")
+      .setTitle("Here is your random cat related Image!")
+      .setDescription(randomItem.description)
       .setImage(randomImage);
 
     await interaction.reply({ embeds: [embed] });
