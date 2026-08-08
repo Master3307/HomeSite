7e1c3705a2a6dbb79b1c423ef5535fe29ec1fd0b
Master3307 <hirmerkorbinian122@gmail.com>
2026-08-08 15:29:53 +0200

    feat(Collect): integrate AboutCard component into Collect page for enhanced user information

--- src/pages/Collect.jsx
+++ src/pages/Collect.jsx
@@ -1,6 +1,8 @@
 import { useTranslation } from 'react-i18next'
 import ThemeSwitch from '../components/ThemeSwitch.jsx'
 import LanguageSwitch from '../components/LanguageSwitch.jsx'
+import About from '../components/AboutCard.jsx'
+
 export default function Home() {
   const { t } = useTranslation(['collect', 'title'])
 
@@ -18,6 +20,7 @@ export default function Home() {
       </div>
 
 
+      <About />
       <ThemeSwitch />
       <LanguageSwitch />
     </>
