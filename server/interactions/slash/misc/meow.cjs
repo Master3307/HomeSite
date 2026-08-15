47588b2368812b49f492f4e0ee0124f8dfc0f302
Master3307 <hirmerkorbinian122@gmail.com>
2026-08-15 09:30:23 +0200

    enhance(meow): add more options that randomly appear

--- server/interactions/slash/misc/meow.cjs
+++ server/interactions/slash/misc/meow.cjs
@@ -1,9 +1,26 @@
 const { SlashCommandBuilder } = require("discord.js");
 
+const meows = [
+  "Meow Meow! <a:CATO_Danse:1533609328433561760>",
+  "Meow Meow! <a:CATO_Danse:1533609328433561760>",
+  "Meow Meow! <a:CATO_Danse:1533609328433561760>",
+  "Meow! <a:1498030996447039672:1538086368457261126>",
+  "Miau Miau! 🇩🇪",
+  "Няв Няв! 🇺🇦",
+  "Meow? Meow!",
+  "*cat noises*",
+  "Purrrrrr…",
+  "Meow Meow!",
+  "meow",
+  "MEOW MEOW!",
+];
+
 module.exports = {
   data: new SlashCommandBuilder().setName("meow").setDescription("Meow?"),
 
   execute(message, args) {
-    message.reply({ content: "Meow Meow! <a:CATO_Danse:1533609328433561760>" });
+    const randomMeow = meows[Math.floor(Math.random() * meows.length)];
+
+    message.reply({ content: randomMeow });
   },
 };
