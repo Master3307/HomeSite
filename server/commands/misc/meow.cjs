2cca31737520d06cb40669754465d879d6e33cca
Master3307 <hirmerkorbinian122@gmail.com>
2026-08-15 09:32:43 +0200

    fix(meow): update the non-slash meow too

--- server/commands/misc/meow.cjs
+++ server/commands/misc/meow.cjs
@@ -1,3 +1,18 @@
+const meows = [
+  "Meow Meow! <a:CATO_Danse:1533609328433561760>",
+  "Meow Meow! <a:CATO_Danse:1533609328433561760>",
+  "Meow Meow! <a:CATO_Danse:1533609328433561760>",
+  "Meow! <a:1498030996447039672:1538086368457261126>",
+  "Miau Miau! 🇩🇪",
+  "Няв Няв! 🇺🇦",
+  "Meow? Meow!",
+  "*cat noises*",
+  "Purrrrrr…",
+  "Meow Meow!",
+  "meow",
+  "MEOW MEOW!",
+];
+
 module.exports = {
   name: "meow",
   description: "Meow?",
@@ -10,9 +25,12 @@ module.exports = {
     "няв-няв",
     "нявняв",
   ],
+
   execute(message, args) {
+    const randomMeow = meows[Math.floor(Math.random() * meows.length)];
+
     message.reply({
-      content: "Meow Meow! <a:CATO_Danse:1533609328433561760>",
+      content: randomMeow,
     });
   },
 };
