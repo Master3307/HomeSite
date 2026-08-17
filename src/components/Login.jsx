f0489da788fbd901e074af4ec2d079de5f87e2c3
Master3307 <hirmerkorbinian122@gmail.com>
2026-08-17 19:20:35 +0200

    fix(login): update Discord authentication link to use a dynamic API URL

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,13 @@
-import { useNavigate } from 'react-router'
 import { useTranslation } from 'react-i18next'
 
+const API_URL = 'https://discord-api.master3307.org'
+
 export default function Cult() {
-  const navigate = useNavigate()
   const { t } = useTranslation('cult')
 
   return (
-    <a href="/api/auth/discord">Continue with Discord</a>
+    <a href={`${API_URL}/auth/discord`}>
+      Continue with Discord
+    </a>
   )
 }
